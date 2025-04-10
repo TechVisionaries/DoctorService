f0a9e7cf84893b53084359635fcfd0a4fc7bc01e
IT21802126-SilvaGMSS <91774321+IT21802126-SilvaGMSS@users.noreply.github.com>
2025-04-10 09:21:58 +0530

    Create index.js
    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middleware/errorHandler");
+
+const app = express();
+const PORT = process.env.PORT || 8050;
+
+// DB connection
+dbConnect();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const doctorRoutes = require("./routes/doctorRoutes");
+app.use("/doctors", doctorRoutes);
+
+
+// Error handlers
+app.use(notFound);
+app.use(errorHandler);
+
+
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server is running  at PORT ${PORT}`);
+});
