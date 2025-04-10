6540370673407b46e28fd69ccc06237bff2d571a
IT21802126-SilvaGMSS <91774321+IT21802126-SilvaGMSS@users.noreply.github.com>
2025-04-10 09:22:17 +0530

    create database connection

---
+++ config/dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1); // Exit app on failure
+  }
+};
+
+module.exports = dbConnect;
