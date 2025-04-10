d0664dbad67dcd160adab675c53f5c29a951f7c0
IT21802126-SilvaGMSS <91774321+IT21802126-SilvaGMSS@users.noreply.github.com>
2025-04-10 09:22:47 +0530

    Create doctorModel.js

---
+++ models/doctorModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  specialization: { type: String, required: true },
+  experience: { type: Number, required: true },
+  availability: { type: String, required: true },
+  hospital: { type: String },
+  imageUrl: { type: String } // optional: image link
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Doctor", doctorSchema);
