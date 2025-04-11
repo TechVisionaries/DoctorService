332266dd0c2b01a326ac24e755c6b19517ccf0dd
IT21802126-SilvaGMSS <91774321+IT21802126-SilvaGMSS@users.noreply.github.com>
2025-04-11 17:58:04 +0530

    Update doctorRoutes.js

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -4,6 +4,8 @@ const {
   getAllDoctors,
   getDoctorById,
   createDoctor,
+  updateDoctor,
+  deleteDoctor
 } = require("../controllers/doctorController");
 
 const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
@@ -11,6 +13,9 @@ const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
 router.get("/", getAllDoctors);
 router.get("/:id", getDoctorById);
 router.post("/", authMiddleware, isAdmin, createDoctor);
+router.put('/:id', isAdmin, updateDoctor);
+router.delete('/:id', isAdmin, deleteDoctor);
+
 
 module.exports = router;
 
